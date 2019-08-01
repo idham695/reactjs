f0edfbcb3f2146dd482cbf3ac81aaa46bf771768
idham <idhamkholid965@gmail.com>
2019-08-01 13:52:24 +0700

    add event onclick

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 // 
-
 function App() {
   return (
     <div className="App">
@@ -66,3 +65,31 @@ export default App;
 // }
 /* <Timer start="0" />
 <Timer start="5" /> */
+
+// event
+
+// class Toogle extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       toogleStatus: true
+//     }
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+
+//   handleClick() {
+//     this.setState(state => ({
+//       toogleStatus: !state.toogleStatus
+//     }))
+//   }
+
+//   render() {
+//     return (
+//       <button onClick={this.handleClick}>
+//         {this.state.toogleStatus ? 'ON' : 'OFF'}
+//       </button>
+//     )
+//   }
+// }
+/* <Toogle /> */
+
