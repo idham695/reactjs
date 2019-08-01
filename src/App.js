9c53860b7cbf7578e33a38fa6d06b4540737da4f
idham <idhamkholid965@gmail.com>
2019-08-01 13:35:04 +0700

    add state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+// Komponen properties
+
+// function Biodata(props) {
+//   return <span> umurnya {props.age} </span>
+// }
+
+// function Greeting(props) {
+//   return <h1> Halo {props.name} - <Biodata age={props.age} /> </h1>
+// }
+
+/* <Greeting name="idham" age="20" />
+        <Greeting name="i" age="25" /> */
+
+
+//  state
+// class Timer extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       time: props.start
+//     }
+//   }
+
+//   // LifeCycle
+//   componentDidMount() {
+//     this.addInterval = setInterval(() => this.increase(), 1000)
+//   }
+
+//   componentWillUnmount() {
+//     clearInterval(this.addInterval)
+//   }
+
+//   increase() {
+//     this.setState((state, props) => ({
+//       time: parseInt(state.time) + 1
+//     }))
+//   }
+
+//   render() {
+//     return (
+//       <div>{this.state.time} detik </div>
+//     )
+//   }
+// }
+/* <Timer start="0" />
+<Timer start="5" /> */
