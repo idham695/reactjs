b54acd939945737f3e404242542c68443847a2b3
idham <idhamkholid965@gmail.com>
2019-08-02 14:09:48 +0700

    add todolist

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import List from './List';
 
 // 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todoItem: '',
+      items: []
+    }
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      items: [...this.state.items, this.state.todoItem],
+      todoItem: ''
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      todoItem: event.target.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.todoItem} onChange={this.handleChange} />
+          <button>Add</button>
+        </form>
+
+
+        <List items={this.state.items} />
+      </div>
+    );
+  }
 }
 
 export default App;
