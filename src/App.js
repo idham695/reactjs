8a84dd5b528add9f369ad508eb07dd6fda8187eb
idham <idhamkholid965@gmail.com>
2019-08-02 14:33:39 +0700

    add API

--- src/App.js
+++ src/App.js
@@ -1,39 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-import List from './List';
 
 // 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      todoItem: '',
-      items: []
+      items: [],
+      isLoading: true
     }
   }
-  handleSubmit = (event) => {
-    event.preventDefault()
-    this.setState({
-      items: [...this.state.items, this.state.todoItem],
-      todoItem: ''
-    })
-  }
 
-  handleChange = (event) => {
-    this.setState({
-      todoItem: event.target.value
-    })
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => this.setState({ items: data, isLoading: false }))
   }
   render() {
+    const { items, isLoading } = this.state
+
+    if (isLoading) {
+      return <p>Loading....</p>
+    }
+
+
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <input value={this.state.todoItem} onChange={this.handleChange} />
-          <button>Add</button>
-        </form>
-
-
-        <List items={this.state.items} />
+        <ul>{items.map((item, index) =>
+          <li key={index}> {item.name}</li>)}</ul>
       </div>
     );
   }
@@ -119,3 +113,82 @@ export default App;
 // }
 /* <Toogle /> */
 
+//  todolist
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       todoItem: '',
+//       items: []
+//     }
+//   }
+//   handleSubmit = (event) => {
+//     event.preventDefault()
+//     this.setState({
+//       items: [...this.state.items, this.state.todoItem],
+//       todoItem: ''
+//     })
+//   }
+
+//   handleChange = (event) => {
+//     this.setState({
+//       todoItem: event.target.value
+//     })
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <form onSubmit={this.handleSubmit}>
+//           <input value={this.state.todoItem} onChange={this.handleChange} />
+//           <button>Add</button>
+//         </form>
+
+
+//         <List items={this.state.items} />
+//       </div>
+//     );
+//   }
+// }
+// function List(props) {
+//   return (
+//       <ul>
+//           {
+//               props.items.map((item, index) => < li key={index}> {item}</li>)
+//           }
+//       </ul >
+//   )
+// }
+
+// using API
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       items: [],
+//       isLoading: true
+//     }
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then(response => response.json())
+//       .then(data => this.setState({ items: data, isLoading: false }))
+//   }
+//   render() {
+//     const { items, isLoading } = this.state
+
+//     if (isLoading) {
+//       return <p>Loading....</p>
+//     }
+
+
+//     return (
+//       <div>
+//         <ul>{items.map((item, index) =>
+//           <li key={index}> {item.name}</li>)}</ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
