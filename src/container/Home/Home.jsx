ab5bc83c4c562713fc2fa5382db8fa27e3e7d76e
idham <idhamkholid965@gmail.com>
2019-09-17 20:19:22 +0700

    add react router

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,51 +1,36 @@
-import React, { Component } from 'react';
-// import YoutubeComp from '../../component/YoutubeComp/YoutubeComp';
-// import Product from '../product/Product';
-// import LifeCycle from '../LifeCycle/LifeCycle';
+import React, { Component, Fragment } from 'react';
+import YoutubeComp from '../../component/YoutubeComp/YoutubeComp';
+import Product from '../product/Product';
+import LifeCycle from '../LifeCycle/LifeCycle';
 import BlogPost from '../BlogPost/BlogPost';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './Home.css';
 
 class Home extends Component {
     state = {
         showComponent: true
     }
     componentDidMount() {
-        // setTimeout(() => {
-        //     this.setState({
-        //         showComponent: false
-        //     })
-        // }, 15000)
+        setTimeout(() => {
+            this.setState({
+                showComponent: false
+            })
+        }, 15000)
     }
     render() {
         return (
-            <div>
-                {/* <p>Youtube Component</p>
-                <hr />
-                <YoutubeComp title="membaca"
-                    desc="20x ditonton. 1 hari yang lalu"
-                />
-                <YoutubeComp title="menulis"
-                    desc="200x ditonton. 3 hari yang lalu"
-                />
-                <YoutubeComp title="menari"
-                    desc="300x ditonton. 5 hari yang lalu"
-                />
-                <YoutubeComp title="menembak"
-                    desc="40x ditonton. 2 hari yang lalu"
-                />
-                <YoutubeComp /> */}
-                {/* <p>Counter</p>
-                <hr />
-                <Product /> */}
-                {/* <p>LifeCycle Component</p>
-                <hr />
-                {
-                    this.state.showComponent ?
-                        <LifeCycle /> : null
-                } */}
-                <p>Blog Post</p>
-                <hr />
-                <BlogPost />
-            </div>
+            <BrowserRouter>
+                <Fragment>
+                    <div className="navigation">
+                        <Link to="/">Blog Post</Link>
+                        <Link to="/product">Product</Link>
+                        <Link to="/lifecycle">LifeCycle</Link>
+                    </div>
+                    <Route path="/" exact component={BlogPost} />
+                    <Route path="/product" component={Product} />
+                    <Route path="/lifecycle" component={LifeCycle} />
+                </Fragment>
+            </BrowserRouter>
         )
     }
 }
